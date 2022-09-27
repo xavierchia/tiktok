4c47db80d58b360f287190fbdf51aef949937cae
xavier <xavier.chia@screeningeagle.com>
2022-09-27 08:28:59 +0800

    add tab bar

--- TikTok/Resources/AppDelegate.swift
+++ TikTok/Resources/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         // this is to set up first VC programatically for > iOS 13 (see SceneDelegate for < iOS13)
         let window = UIWindow(frame: UIScreen.main.bounds)
-        window.rootViewController = HomeViewController()
+        window.rootViewController = TabBarViewController()
         self.window = window
         self.window?.makeKeyAndVisible()
         
