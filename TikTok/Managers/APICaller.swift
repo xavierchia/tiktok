798d97e53fd1f34d7f07dba9d4def64a5817a11c
xavier <xavier.chia@screeningeagle.com>
2022-09-28 09:26:18 +0800

    add functionality to managers

--- TikTok/Managers/APICaller.swift
+++
@@ -1,8 +0,0 @@
-//
-//  APICaller.swift
-//  TikTok
-//
-//  Created by Xavier Chia on 23/9/22.
-//
-
-import Foundation
--- TikTok/Managers/StorageManager.swift
+++ TikTok/Managers/StorageManager.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+import FirebaseStorage
+
+final class StorageManager {
+    public static let shared = StorageManager()
+    
+    private let database = Storage.storage().reference()
+    
+    private init() {}
+    
+    // Public
+    
+    public func getVideoURL(completion: (URL) -> Void) {
+
+    }
+}
